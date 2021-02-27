4bb99022fc5d8319d2164d4f23c14f10cafa4b09
Carlos Solano <carlos.solanogomez@hotmail.com>
2021-02-27 11:58:43 +0100

    clonar elemento de personaje dummy

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// Clonado del Elemento de personaje-dummy
+
+const persona = document.getElementById("personaje-dummy");
+const personaje = persona.children;
+
+const dummy = personaje[1];
+const dummyCopia = dummy.cloneNode(true);
+
+console.log(dummyCopia);
+
+div.textContent;
