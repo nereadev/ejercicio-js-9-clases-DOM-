83a2f9e6b87611df263ebb61c73412ad3c59b3e9
Nerea García <nereagarcia.c@gmail.com>
2021-03-01 00:06:09 +0100

    Accion matar personaje con boton morir

--- js/index.js
+++ js/index.js
@@ -1,64 +1,69 @@
-// Funcion 1.1 Clonado del Elemento de personaje-dummy
+/* eslint-disable no-loop-func */
 for (const persona of totalPersonajes) {
   setTimeout(() => {
     const claseDummy = document.querySelector(".personaje-dummy").cloneNode(true);
-
-    // const dummy = document.querySelector(".personaje-dummy");
-    //const dummyCopia = dummy.cloneNode(true);
-
-    //console.log(dummyCopia);
-
-    // Funcion 1.2 Eliminacion de clase
-
-    //dummyCopia.classList.remove("personaje-dummy");
-    //console.log(dummyCopia);
     const estatuto = document.createElement("i");
     estatuto.classList.add("fas");
+
     claseDummy.classList.remove("personaje-dummy");
     claseDummy.querySelector(".nombre").textContent = `${persona.nombre} ${persona.familia}`;
     claseDummy.querySelector(".estado").appendChild(estatuto);
     claseDummy.querySelector(".edad").textContent = `Edad: ${persona.edad}`;
     claseDummy.querySelector(".estado").appendChild(estatuto);
-
-    // Funcion 1.3 Alimentacion del HTML con los personajes
-
     const imgActor = claseDummy.querySelector("img");
     imgActor.src = `img/${persona.nombre.toLowerCase()}.jpg`;
     imgActor.alt = `${persona.nombre} ${persona.familia}`;
-    if (persona.estado === "vivo") {
-      estatuto.classList.add("fa-thumbs-up");
-      imgActor.classList.remove("muerte");
-    } else {
-      estatuto.classList.add("fa-thumbs-down");
-      imgActor.classList.add("muerte");
+    const botonMuere = claseDummy.querySelector(".muere");
 
+    function vivirMorir() {
+      if (persona.estado === "vivo") {
+        estatuto.classList.add("fa-thumbs-up");
+        imgActor.classList.remove("muerte");
+      } else {
+        estatuto.classList.add("fa-thumbs-down");
+        imgActor.classList.add("muerte");
+      }
     }
+
+    vivirMorir();
+
     if (persona instanceof Rey) {
       const reinado = document.createElement("li");
       reinado.classList.add("reinadoAnual");
-      reinado.textContent =
-        `Años que lleva de reinado: ${persona.tiempoReinado}`;
+      reinado.textContent = `Años que lleva de reinado: ${persona.tiempoReinado}`;
       claseDummy.querySelector(".personaje-overlay").querySelector(".metadata").append(reinado);
-      claseDummy.querySelector(".emoji").textContent = `👑`;
+      claseDummy.querySelector(".emoji").textContent = "👑";
+      botonMuere.addEventListener("click", () => {
+        persona.morir();
+        vivirMorir();
+      });
     }
 
     if (persona instanceof Luchador) {
       const armas = document.createElement("li");
       armas.classList.add("arma");
-      armas.textContent = `Arma: ${persona.arma}`
+      armas.textContent = `Arma: ${persona.arma}`;
       claseDummy.querySelector(".personaje-overlay").querySelector(".metadata").append(armas);
       const atributo = document.createElement("li");
       atributo.classList.add("destreza");
       atributo.textContent = `Destreza: ${persona.destreza}`;
       claseDummy.querySelector(".personaje-overlay").querySelector(".metadata").append(atributo);
-      claseDummy.querySelector(".emoji").textContent = `🗡`;
+      claseDummy.querySelector(".emoji").textContent = "🗡";
+      botonMuere.addEventListener("click", () => {
+        persona.morir();
+        vivirMorir();
+      });
     }
 
     if (persona instanceof Asesor) {
       const asesoramiento = document.createElement("li");
       asesoramiento.textContent = `Asesoro de: ${persona.personajeAsesoro}`;
       claseDummy.querySelector(".personaje-overlay").querySelector(".metadata").append(asesoramiento);
-      claseDummy.querySelector(".emoji").textContent = `🎓`;
+      claseDummy.querySelector(".emoji").textContent = "🎓";
+      botonMuere.addEventListener("click", () => {
+        persona.morir();
+        vivirMorir();
+      });
     }
 
     if (persona instanceof Escudero) {
@@ -68,11 +73,13 @@ for (const persona of totalPersonajes) {
       const servicio = document.createElement("li");
       servicio.textContent = `A servicio de: ${persona.personajeQueSirve}`;
       claseDummy.querySelector(".personaje-overlay").querySelector(".metadata").append(servicio);
-
-      claseDummy.querySelector(".emoji").textContent = `🛡`;
+      claseDummy.querySelector(".emoji").textContent = "🛡";
+      botonMuere.addEventListener("click", () => {
+        persona.morir();
+        vivirMorir();
+      });
     }
 
-
-    document.querySelector(".personajes").append(claseDummy)
+    document.querySelector(".personajes").append(claseDummy);
   }, 1000 * (totalPersonajes.findIndex((personal) => personal === persona) + 1));
 }
